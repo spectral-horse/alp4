accfd1a053303208c0d027342c529fb51a407a02
spectral_horse <>
2025-09-05 16:14:52 +0100

    Organise into multiple files

---
+++ alp.py
@@ -0,0 +1,137 @@
+from .error import AlpError
+from ._consts import *
+from ctypes import CDLL, c_long, c_void_p, byref
+import numpy as np
+
+
+
+class Alp:
+    _lib = None
+
+    def __init__(self, library_path):
+        if self._lib is not None:
+            raise RuntimeError("ALP already initialised")
+
+        type(self)._lib = CDLL(library_path)
+
+    def _call(self, func_name, *args):
+        ret = self._lib[func_name](*args)
+
+        if ret != ALP_OK.value: raise AlpError(ret)
+
+    def open_device(self, device_num = ALP_DEFAULT.value):
+        dev = AlpDevice()
+        dev._id = c_long(0)
+        dev._alp = self
+
+        self._call(
+            "AlpDevAlloc",
+            c_long(device_num), c_long(0), byref(dev._id)
+        )
+
+        return dev
+
+class AlpDevice:
+    def _inquire(self, inquiry, proj = False):
+        func_name = "AlpProjInquire" if proj else "AlpDevInquire"
+        value = c_long(0)
+
+        self._alp._call(func_name, self._id, inquiry, byref(value))
+
+        return value
+
+    def _control(self, param, value):
+        self._alp._call(
+            "AlpProjControl",
+            self._id, param, value
+        )
+
+    def get_display_size(self):
+        width = self._inquire(ALP_DEV_DISPLAY_WIDTH).value
+        height = self._inquire(ALP_DEV_DISPLAY_HEIGHT).value
+
+        return width, height
+
+    def allocate_sequence(self, bitplanes, images):
+        seq = AlpSequence()
+        seq._alp = self._alp
+        seq._id = c_long(0)
+        seq._dev = self
+
+        self._alp._call(
+            "AlpSeqAlloc",
+            self._id, c_long(bitplanes), c_long(images), byref(seq._id)
+        )
+
+        return seq
+
+    def halt(self):
+        self._alp._call("AlpDevHalt", self._id)
+
+    def wait(self):
+        self._alp._call("AlpProjWait", self._id)
+
+    def is_projecting(self):
+        code = self._inquire(ALP_PROJ_STATE, proj = True)
+
+        if code.value == ALP_PROJ_ACTIVE.value: return True
+        elif code.value == ALP_PROJ_IDLE.value: return False
+        else:
+            raise RuntimeError("AlpProjInquire gave invalid ALP_PROJ_STATE")
+
+    def set_trigger(self, trigger_type):
+        self._control(ALP_PROJ_STEP, trigger_type.value)
+
+class AlpSequence:
+    _formats = {
+        "msb_align": ALP_DATA_MSB_ALIGN,
+        "lsb_align": ALP_DATA_LSB_ALIGN,
+        "binary_topdown": ALP_DATA_BINARY_TOPDOWN,
+        "binary_bottomup": ALP_DATA_BINARY_BOTTOMUP
+    }
+
+    def _control(self, param, value):
+        self._alp._call(
+            "AlpSeqControl",
+            self._dev._id, self._id, param, value
+        )
+
+    def free(self):
+        self._alp._call("AlpSeqFree", self._dev._id, self._id)
+
+    def set_timing(
+        self,
+        illuminate = ALP_DEFAULT.value, picture = ALP_DEFAULT.value,
+        sync_delay = 0, sync_pulse_width = ALP_DEFAULT.value, trigger_delay = 0
+    ):
+        self._alp._call(
+            "AlpSeqTiming", self._dev._id, self._id,
+            c_long(int(illuminate)), c_long(int(picture)),
+            c_long(int(sync_delay)), c_long(int(sync_pulse_width)),
+            c_long(int(trigger_delay))
+        )
+
+    def set_format(self, fmt):
+        fmt = self._formats.get(fmt)
+
+        if fmt is None:
+            raise ValueError("valid formats are "+", ".join(_formats.keys()))
+
+        self._control(ALP_DATA_FORMAT, fmt)
+
+    def put(self, offset, n, data):
+        data = np.ascontiguousarray(data)
+
+        self._alp._call(
+            "AlpSeqPut",
+            self._dev._id, self._id, c_long(offset), c_long(n),
+            c_void_p(data.__array_interface__["data"][0])
+        )
+
+    def set_cycles(self, n):
+        self._control(ALP_SEQ_REPEAT, c_long(n))
+
+    def start(self, continuous = False):
+        func_name = "AlpProjStartCont" if continuous else "AlpProjStart"
+
+        self._alp._call(func_name, self._dev._id, self._id)
---
+++ error.py
@@ -0,0 +1,26 @@
+class AlpError(RuntimeError):
+    _err_codes = {
+        1001: "NOT_ONLINE",
+        1002: "NOT_IDLE",
+        1003: "NOT_AVAILABLE",
+        1004: "NOT_READY",
+        1005: "PARM_INVALID",
+        1006: "ADDR_INVALID",
+        1007: "MEMORY_FULL",
+        1008: "SEQ_IN_USE",
+        1009: "HALTED",
+        1010: "ERROR_INIT",
+        1011: "ERROR_COMM",
+        1012: "DEVICE_REMOVED",
+        1013: "NOT_CONFIGURED",
+        1014: "LOADER_VERSION",
+        1018: "ERROR_POWER_DOWN",
+        1019: "DRIVER_VERSION",
+        1020: "SDRAM_INIT"
+    }
+
+    def __init__(self, code):
+        if code == _ALP_OK.value:
+            raise ValueError("code 0 is ALP_OK, not an error")
+
+        super().__init__(self._err_codes.get(code, "UNKNOWN"))
