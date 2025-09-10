5872f4970c51ca790e8e4ad72e9ed8cedf5f8fa2
spectral-horse <230595758+spectral-horse@users.noreply.github.com>
2025-09-10 13:45:18 +0100

    Added context manager capabilities

--- __main__.py
+++ __main__.py
@@ -1,16 +1,17 @@
 from . import *
+from contextlib import ExitStack
 
 
 
+with ExitStack() as stack:
+    alp = stack.enter_context(Alp("./alp4395.dll"))
 
-alp = Alp("./alp4395.dll")
+    try:
+        dmd = stack.enter_context(alp.open_device())
+    except AlpError:
+        print("Couldn't open DMD:", *e.args)
+        exit(1)
 
-try:
-    dmd = alp.open_device()
-except AlpError:
-    print("Couldn't open DMD:", *e.args)
-    exit(1)
+    width, height = dmd.get_display_size()
 
-width, height = dmd.get_display_size()
-
-print(f"Found DMD of size {width}x{height}")
+    print(f"Found DMD of size {width}x{height}")
